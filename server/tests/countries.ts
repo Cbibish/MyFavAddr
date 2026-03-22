492f91b94a2625b8ba19da18a40593c5052a2a96
Cbibish <sttribet@gmail.com>
2026-03-22 13:29:24 +0100

    included the front feature

--- server/tests/countries.ts
+++ server/tests/countries.ts
@@ -1,6 +1,7 @@
 export async function getCountriesStartingWith(srch: string): Promise<string[]> {
   const response = await fetch('https://api.first.org/data/v1/countries?limit=1000');
   const json = await response.json();
+  
   return Object.values(json.data)
     .map((c: any) => c.country as string)
     .filter(name => name.toLowerCase().startsWith(srch.toLowerCase()));
